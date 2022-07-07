aaffb75b6b7cef7d36862112dadccd550873bb3d
imizallah <ishayasunday@gmail.com>
2022-07-07 10:40:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mystic goright"></div>
+      <div className="mystic goleft"></div>
+
+      <h2>Just testing the social app</h2>
     </div>
   );
 }
---
+++ src/pages/Timeline/Timeline.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Timeline.css'
+
+const Timeline = () => {
+  return (
+    <div className="timeline">
+      {/* Left Side */}
+
+      {/* Posts Side */}
+
+      {/* Right Side */}
+    </div>
+  )
+}
+
+export default Timeline
